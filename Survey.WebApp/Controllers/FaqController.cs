624a2e149652730fed43858986376558f5869d22
Việt Trần <viettd1190@gmail.com>
2018-09-19 16:45:05 +0700

    Faq

--- Survey.WebApp/Controllers/FaqController.cs
+++ Survey.WebApp/Controllers/FaqController.cs
@@ -1,17 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using Survey.Service;
+using Survey.WebApp.Models;
 
 namespace Survey.WebApp.Controllers
 {
-    public class FaqController : Controller
+    public class FaqController : BaseController
     {
+        private IFaqService _faqService;
+
+        public FaqController(IFaqService faqService)
+        {
+            _faqService = faqService;
+        }
+
         // GET: Faq
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetList(string q,
+                                    int pageNumber,
+                                    int pageSize)
+        {
+            List<FaqModel> list = new List<FaqModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(new FaqModel
+                         {
+                                 Id = i + 1,
+                                 Title = "HƯỚNG DẪN ĐẶT HÀNG",
+                                 Content = "Đế Sạc Không Dây Tronsmart WC01 Chuẩn Qi 10W Type-C (Không Kèm Adapter) tương thích với công nghệ sạc không dây Qi: Hỗ trợ hầu hết các điện thoại thông minh."
+                         });
+            }
+
+            return Json(list,
+                        "json");
         }
     }
 }
---
+++ Survey.WebApp/Models/FaqModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Survey.WebApp.Models
+{
+    public class FaqModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Câu hỏi không được để trống")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Nội dung trả lời không được để trống")]
+        public string Content { get; set; }
+
+        public bool IsDisplay { get; set; }
+    }
+}
